6fcecf3e03d62428291f22984ee891aa9d0f8bcc
Christian strube <christianstrube@icloud.com>
2024-07-27 22:00:09 +0000

    chore: Update React dependencies and service worker registration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-serviceWorkerRegistration.register();
+// Ändere dies:
+serviceWorker.register();
